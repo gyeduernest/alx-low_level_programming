13b08a05f7661716b58197ba1a8469d75e56af3c
gyeduernest <gyeduernest@gmail.com>
2023-02-17 13:28:51 -0800

    tired of doing this

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
 /**
  * main - This is the main function
  *
- * Return: 0
+ * Return:0;
  */
 
 int main(void)
-{	printf ("with proper grammer,but the outcome is a piece of art,"\n);
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 	}
