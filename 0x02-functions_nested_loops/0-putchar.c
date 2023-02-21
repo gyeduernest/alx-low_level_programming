a5559b65b302c7e303fa8352efc77a4107c9baaf
gyeduernest <gyeduernest@gmail.com>
2023-02-21 12:17:36 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**main function of the assignments
+ * Return: Always 0 success
+ */
+
+int main(){
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return 0;
+}
