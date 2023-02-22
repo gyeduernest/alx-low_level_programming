e7a715c2cd4ddcb74af78cef010f0c35d1c1bdf6
gyeduernest <gyeduernest@gmail.com>
2023-02-22 10:59:33 -0800

    haha

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
-
-/**main function of the assignments
- * Return: Always 0 success
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
-int main(){
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return 0;
 }
